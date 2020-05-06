6ac0a0a3ad83dce4c16da26818f69abbe9e647f0
Stephan <sgriesel@gmail.com>
2020-05-06 06:59:18 +0200

    add markkup & fix typo

--- public/js/load-products.js
+++ public/js/load-products.js
@@ -1,4 +1,4 @@
-export async function loadProduct() {
+export async function loadProducts() {
     const data = await fetch('/.netlify/functions/get-products')
         .then((res) => res.json())
         .catch((err) => console.error(err));
