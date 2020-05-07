f779f92bc25b5ed52a04c55bb09343327f9cbe06
Stephan <sgriesel@gmail.com>
2020-05-07 06:27:06 +0200

    inject data data to dom

--- functions/get-products.js
+++ functions/get-products.js
@@ -1,5 +1,7 @@
 const products = require('./data/products.json');
 
+// Load product JSON from data file, exports a handler function that returns status code of 200 and product data as JSON-encoded string
+
 exports.handler = async () => {
     return {
         statusCode: 200,
--- public/js/load-products.js
+++ public/js/load-products.js
@@ -1,3 +1,23 @@
+function createProductFromTemplate(item) {
+    const template = document.querySelector('#product');
+    const product = template.content.cloneNode(true);
+
+    product.querySelector('h2').innerText = item.name;
+    product.querySelector('.description').innerText = item.description;
+    product.querySelector('[name=sku]').value = item.sku;
+    product.querySelector('.price').innerText = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: item.currency,
+    }).format((item.amount / 100).toFixed(2));
+
+    const img = product.querySelector('img');
+    img.src = item.image;
+    img.alt = item.name;
+
+    return product;
+
+}
+
 export async function loadProducts() {
     const data = await fetch('/.netlify/functions/get-products')
         .then((res) => res.json())
